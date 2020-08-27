263ba3691255455ab8970cf2ed532645ea95bcae
Neelesh Shetty <an.neelesh@novasemita.org>
2020-08-27 17:26:25 +0530

    created redux store

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
