13ef8b97b58048916de8333fbeb2dc63c65bec01
Neelesh Shetty <an.neelesh@novasemita.org>
2020-09-03 13:42:00 +0530

    new container with input payload

---
+++ src/components/NewCakeContainer.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import buyCake from '../redux/cake/cakeActions'
+import { connect } from 'react-redux'
+
+function NewCakeContainer(props) {
+
+    const [number, setNumber] = useState(1)
+
+    return (
+        <div>
+            <h2>Number of Cakes : <span>{props.numOfCakes}</span></h2>
+            <input type="text" value={number} onChange={e => setNumber(e.target.value)} />
+            <button onClick={() => props.buyCake(number)}>Buy {number} Cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.cake.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: (number) => dispatch(buyCake(number))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCakeContainer)
